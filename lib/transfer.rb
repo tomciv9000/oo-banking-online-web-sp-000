553e9cef8cb38f4e4ec23e8d367b7d7a5ec5c651
Tom White <eugene.white@gmail.com>
2019-06-12 23:58:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  
+  
 end
