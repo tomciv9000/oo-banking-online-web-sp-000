f5af54ef0cf95d1fd332eb91416a68744b0c8195
Tom White <eugene.white@gmail.com>
2019-06-12 23:46:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -19,7 +19,10 @@ class BankAccount
   end
   
   def valid?
-    true if @balance > 0 && @status = "open"
+    if @balance > 0 && @status = "open"
+      return true 
+    else false 
+    end
   end
   
 end
