180821d43557edb20f01a49dd7c26e6d43d760bb
Tom White <eugene.white@gmail.com>
2019-06-13 00:21:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? && @status == "pending"
+    if @sender.balance >= @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
