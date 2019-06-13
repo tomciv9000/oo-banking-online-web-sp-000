21a739cb574d7319d143f53c7b2ccff5a1214a1c
Tom White <eugene.white@gmail.com>
2019-06-13 00:26:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
     end
   end
   
-  def reverse_transaction
+  def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
