0c11e6300c8bb670a48e9b2335abba8385387a63
Tom White <eugene.white@gmail.com>
2019-06-13 00:22:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
